722158072e163930200465c97080aa1f6d70beec
hina2113 <110330118+hinaplugin@users.noreply.github.com>
2025-02-24 01:59:49 +0900

    C

--- script.js
+++ script.js
@@ -1,22 +1,34 @@
 function generateTemplate() {
     const name = document.getElementById('onamae').value;
-    const planning = document.getElementById('planning').value;
-    const forum = document.getElementById('forum').value;
-    const time = document.getElementById('time').value;
-    const version = document.getElementById('version').value;
-    const isCheckNormal = document.getElementById('normal').checked;
-    const isCheckFlat = document.getElementById('flat').checked;
-    const isCheckAmplified = document.getElementById('amplified').checked;
-    const isCheckVoid = document.getElementById('void').checked;
+    const user = document.getElementById('yu-za-').value;
+    const date = document.getElementById('start').value;
+    const project = document.getElementById('pj').value;
+    const mokuteki = document.getElementById('mokuteki').value;
+    const isCheckLogo = document.getElementById('lo').checked;
+    const isCheckBanner = document.getElementById('banner').checked;
+    const isCheckIcon = document.getElementById('icon').checked;
+    const isCheckFly = document.getElementById('fly').checked;
     const isCheckOther = document.getElementById('etc').checked;
-    const other = document.getElementById('other').value;
-    const isOp = document.getElementById('op').checked;
-    const op = document.getElementById('opuser').value;
-    const isWhitelist = document.getElementById('whitelist').checked;
-    const whitelist = document.getElementById('whitelistuser').value;
-    const isPanel = document.getElementById('panel').checked;
-    const panel = document.getElementById('paneluser').checked;
-    const plugin = document.getElementById('plugin').value;
+    const other = document.getElementById('sonota1').value;
+    const size = document.getElementById('size').value;
+    const isPng = document.getElementById('png').checked;
+    const isJpeg = document.getElementById('jpg').checked;
+    const isEtc = document.getElementById('sonota').checked;
+    const other2 = document.getElementById('sonota2').value;
+    const color = document.getElementById('color').value;
+    const style = document.getElementById('style').value;
+    const text = document.getElementById('text').value;
+    const logo = document.getElementById('logo').value;
+    const sankou = document.getElementById('sankou').value;
+    const time = document.getElementById('meeting-time').value;
+    const bikou3 = document.getElementById('bikou3').value;
+    const youbou = document.getElementById('youbou').value;
+    const isYes = document.getElementById('yes').checked;
+    const isNo = document.getElementById('no').checked;
+    const bikou = document.getElementById('bikou').value;
+    const isHai = document.getElementById('hai').checked;
+    const isIie = document.getElementById('iie').checked;
+    const bikou2 = document.getElementById('bikou2').value;
     const comment = document.getElementById('comment').value;
 
     let error = false;
@@ -27,57 +39,79 @@ function generateTemplate() {
         error = true;
     }
 
-    if (planning.length == 0) {
+    if (user.length == 0) {
         if (errorMessage.length != 0) {
             errorMessage += "\n";
         }
-        errorMessage += "企画名は必須項目です．";
+        errorMessage += "ユーザーIDは必須項目です．";
         error = true;
     }
 
-    if (version.length == 0) {
+    if (project.length == 0) {
         if (errorMessage.length != 0) {
             errorMessage += "\n";
         }
-        errorMessage += "サーバーバージョンは必須項目です．";
+        errorMessage += "プロジェクト名は必須項目です．";
         error = true;
     }
 
-    if (!isCheckNormal && !isCheckFlat && !isCheckAmplified && !isCheckVoid && !isCheckOther) {
+    if (mokuteki.length == 0) {
         if (errorMessage.length != 0) {
             errorMessage += "\n";
         }
-        errorMessage += "ワールドタイプを選択してください．";
+        errorMessage += "デザインの目的は必須項目です．";
+        error = true;
+    }
+
+    if (!isCheckLogo && !isCheckBanner && !isCheckIcon && !isCheckFly && !isCheckOther) {
+        if (errorMessage.length != 0) {
+            errorMessage += "\n";
+        }
+        errorMessage += "必要なデザイン物を選択してください．";
         error = true;
     }else if (isCheckOther && other.length == 0){
         if (errorMessage.length != 0) {
             errorMessage += "\n";
         }
-        errorMessage += "配布ワールドの種類またはURLを入力してください．";
+        errorMessage += "その他のデザイン物の種類を入力してください．";
         error = true;
     }
 
-    if (isOp && op.length == 0) {
+    if (size.length == 0) {
         if (errorMessage.length != 0) {
             errorMessage += "\n";
         }
-        errorMessage += 'OPのユーザー名を入力してください．';
+        errorMessage += "デザインのサイズは必須項目です．";
         error = true;
     }
 
-    if (isWhitelist && whitelist.length == 0) {
+    if (!isPng && !isJpeg && !isEtc) {
         if (errorMessage.length != 0) {
             errorMessage += "\n";
         }
-        errorMessage += 'ホワイトリストのユーザー名を入力してください．';
+        errorMessage += "必要なデザインの形式を選択してください．";
+        error = true;
+    }else if (isEtc && other2.length == 0) {
+        if (errorMessage.length != 0) {
+            errorMessage += "\n";
+        }
+        errorMessage += "その他のデザイン形式の種類を入力してください．";
         error = true;
     }
 
-    if (isPanel && panel.length == 0) {
+    if (!isYes && !isNo) {
         if (errorMessage.length != 0) {
             errorMessage += "\n";
         }
-        errorMessage += 'パネルに追加するユーザーを入力してください．';
+        errorMessage += 'アイデアの提案が必要か選択してください．';
+        error = true;
+    }
+
+    if (!isHai && !isIie) {
+        if (errorMessage.length != 0) {
+            errorMessage += "\n";
+        }
+        errorMessage += '製作開始の依頼を選択してください．';
         error = true;
     }
 
@@ -86,70 +120,112 @@ function generateTemplate() {
         return;
     }
 
-    let forumlink = "";
-    if (forum.length > 0) {
-        forumlink += forum;
-    }else {
-        forumlink += "なし";
+    let target = "";
+    if (isCheckLogo) {
+        target += "ロゴ";
     }
 
-    let worldType = "";
-    if (isCheckNormal) {
-        worldType += "ノーマル";
+    if (isCheckBanner) {
+        if (target.length > 0) {
+            target += ", バナー";
+        }else{
+            target += "バナー";
+        }
     }
 
-    if (isCheckFlat) {
-        worldType += "フラット";
+    if (isCheckIcon) {
+        if (target.length > 0) {
+            target += ", アイコン";
+        }else{
+            target += "アイコン";
+        }
     }
 
-    if (isCheckAmplified) {
-        worldType += "アンプリファイド";
-    }
-
-    if (isCheckVoid) {
-        worldType += "ボイド";
+    if (isCheckFly) {
+        if (target.length > 0) {
+            target += ", フライヤー";
+        }else{
+            target += "フライヤー";
+        }
     }
 
     if (isCheckOther) {
-        worldType += `配布ワールド`
+        if (target.length > 0) {
+            target += `, その他：${other}`
+        }else{
+            target += `その他：${other}`
+        }
     }
 
-    const output = `### 単発鯖貸し出し申請書
+    let targetType = "";
+    if (isPng) {
+        targetType += "png";
+    }
+
+    if (isJpeg) {
+        if (targetType.length > 0) {
+            targetType += ", jpeg";
+        }else{
+            targetType += "jpeg";
+        }
+    }
+
+    if (isEtc) {
+        if (targetType.length > 0) {
+            targetType += `, その他：${other2}`
+        } else{
+            targetType += `その他：${other2}`
+        }
+    }
+
+    const output = `### デザイン制作依頼書
     
-申請者： [${name}]
+依頼者名： [${name}]
+ユーザーID(数字のみ)： [${user}]
+依頼日： [${date}]
     
-1. 企画名：
-    - [${planning}]
+1. プロジェクト名：
+    - [${project}]
     
-2. 企画発足フォーラムへのリンク：
-    - [${forum}]
+2. デザインの目的：
+    - [${mokuteki}]
     
-3. 申請期間：
-    - [${time.replace("T", " ")}]
+3. 必要なデザイン物：
+    - [${target}]
     
-4. サーバーバージョン：
-    - [${version}]
+4. デザインのサイズと形式：
+    - サイズ： [${size}]
+    - 形式： [${targetType}]
     
-5. ワールドタイプ
-    - [${worldType}]
-    - [${other.length > 0 ? other : "指定なし"}]
+5. カラーパレット
+    - [${color.length > 0 ? color : "指定なし"}]
     
-6. OP
-    - [${isOp ? "使用する" : "使用しない"}]
-    - [${isOp ? op : "指定なし"}]
+6. デザインのスタイル
+    - [${style.length > 0 ? style : "指定なし"}]
     
-7. ホワイトリスト
-    - [${isWhitelist ? "使用する" : "使用しない"}]
-    - [${isWhitelist ? whitelist : "指定なし"}]
+7. 必要な要素
+    - テキスト: [${text.length > 0 ? text : "指定なし"}]
+    - ロゴ： [${logo.length > 0 ? logo : "指定なし"}]
     
-8. パネル
-    - [${isPanel ? "使用する" : "使用しない"}]
-    - [${isPanel ? panel : "指定なし"}]
+8. 参考例
+    - [${sankou.length > 0 ? sankou : "なし"}]
     
-9. プラグイン
-    - [${plugin.length > 0 ? plugin : "指定なし"}]
+9. 納期
+    - 納期: [${time.replace("T", " ")}]
+    - 備考: [${bikou3.length > 0 ? bikou3 : "なし"}]
     
-10. コメントや質問
+10. 追加の要望
+    - [${youbou.length > 0 ? youbou : "なし"}]
+    
+11. デザイン案の提案
+    - デザイン案の提案： [${isYes ? "はい" : "いいえ"}]
+    - 備考： [${bikou.length > 0 ? bikou : "なし"}]
+    
+12. 製作開始の依頼
+    - 製作開始： [${isHai ? "はい" : "いいえ"}]
+    - 備考: [${bikou2.length > 0 ? bikou2 : "なし"}]
+    
+13. 追加のコメント
     - [${comment.length > 0 ? comment : "なし"}]`
 
     document.getElementById('output').value = output;
